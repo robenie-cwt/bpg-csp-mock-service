372313a5625024cfd766dee3d55dd7b61d973fb4
Robenie Fernandez <rfernandez@mycwt.com>
2022-10-28 17:49:30 +0800

    Propagate scenario state before response is sent to the caller

--- src/main/java/com/github/tomakehurst/wiremock/extension/pubsub/ScenarioStateStubRequestHandler.java
+++ src/main/java/com/github/tomakehurst/wiremock/extension/pubsub/ScenarioStateStubRequestHandler.java
@@ -3,17 +3,14 @@ package com.github.tomakehurst.wiremock.extension.pubsub;
 import com.github.tomakehurst.wiremock.common.DataTruncationSettings;
 import com.github.tomakehurst.wiremock.core.Admin;
 import com.github.tomakehurst.wiremock.core.StubServer;
-import com.github.tomakehurst.wiremock.core.WireMockApp;
 import com.github.tomakehurst.wiremock.extension.PostServeAction;
 import com.github.tomakehurst.wiremock.extension.requestfilter.RequestFilter;
 import com.github.tomakehurst.wiremock.http.Response;
 import com.github.tomakehurst.wiremock.http.ResponseRenderer;
 import com.github.tomakehurst.wiremock.http.StubRequestHandler;
-import com.github.tomakehurst.wiremock.http.StubResponseRenderer;
 import com.github.tomakehurst.wiremock.stubbing.Scenario;
 import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
 import com.github.tomakehurst.wiremock.verification.RequestJournal;
-
 import java.util.List;
 import java.util.Map;
 
@@ -21,23 +18,31 @@ public class ScenarioStateStubRequestHandler extends StubRequestHandler {
 
   private final CommandPublisher publisher;
 
-  public ScenarioStateStubRequestHandler(StubServer stubServer,
-                                         ResponseRenderer responseRenderer,
-                                         Admin admin,
-                                         Map<String, PostServeAction> postServeActions,
-                                         RequestJournal requestJournal,
-                                         List<RequestFilter> requestFilters,
-                                         boolean loggingDisabled,
-                                         DataTruncationSettings dataTruncationSettings,
-                                         CommandPublisher publisher) {
-    super(stubServer,responseRenderer,admin,postServeActions,requestJournal,requestFilters, loggingDisabled,
-            dataTruncationSettings);
-    this.publisher=publisher;
+  public ScenarioStateStubRequestHandler(
+      StubServer stubServer,
+      ResponseRenderer responseRenderer,
+      Admin admin,
+      Map<String, PostServeAction> postServeActions,
+      RequestJournal requestJournal,
+      List<RequestFilter> requestFilters,
+      boolean loggingDisabled,
+      DataTruncationSettings dataTruncationSettings,
+      CommandPublisher publisher) {
+    super(
+        stubServer,
+        responseRenderer,
+        admin,
+        postServeActions,
+        requestJournal,
+        requestFilters,
+        loggingDisabled,
+        dataTruncationSettings);
+    this.publisher = publisher;
   }
 
   @Override
   protected void beforeResponseSent(ServeEvent serveEvent, Response response) {
-    if (!serveEvent.getWasMatched()) {
+    if (!serveEvent.getWasMatched() || serveEvent.getStubMapping() == null) {
       super.beforeResponseSent(serveEvent, response);
       return;
     }
@@ -49,10 +54,10 @@ public class ScenarioStateStubRequestHandler extends StubRequestHandler {
     }
 
     admin.getAllScenarios().getScenarios().stream()
-            .filter(s -> s.getName().equals(scenarioName))
-            .map(Scenario::getState)
-            .findAny()
-            .ifPresent(state -> setState(scenarioName, state));
+        .filter(s -> s.getName().equals(scenarioName))
+        .map(Scenario::getState)
+        .findAny()
+        .ifPresent(state -> setState(scenarioName, state));
     super.beforeResponseSent(serveEvent, response);
   }
 
